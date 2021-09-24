0d640a9167b42a1963caa097286448868ad871fe
Diego Rodriguez <diegof.rodriguezc@ecci.edu.co>
2021-09-24 10:31:35 -0500

    1013

--- RubyBasico/S11_Strings.rb
+++ RubyBasico/S11_Strings.rb
@@ -1,1 +1,11 @@
+#String sin interpolacion
+puts 'Ahora son las #{Time.now}'
 
+#String con interpolacion
+puts "Ahora son las #{Time.now}"
+
+#String sin soporte de secuencias de escape
+puts 'Ahora son las #{Time.now}'
+
+#String con soporte de secuencias de escape
+puts "Ahora \n son las #{Time.now}"
